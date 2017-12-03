e00b6540569055189f0f014652edbaf6d2abd3e8
Jason Turan <turan.jason@gmail.com>
2017-12-03 16:30:30 -0600

    Test

--- src/main/java/com/dbs/sae/training/nerddinner/controller/HomeController.java
+++ src/main/java/com/dbs/sae/training/nerddinner/controller/HomeController.java
@@ -2,13 +2,12 @@ package com.dbs.sae.training.nerddinner.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.security.Principal;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    String index() { return "homeIndex";    }
+    String index() {
+        return "home";
+    }
 
 }
