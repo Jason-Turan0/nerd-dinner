9f94eb8533a9814083611a6b7ed4f28c7f008801
jturan <turan.jason@gmail.com>
2017-12-17 23:12:54 -0600

    Good progress on account creation screen.

--- src/main/java/com/dbs/sae/training/nerddinner/controller/LoginController.java
+++ src/main/java/com/dbs/sae/training/nerddinner/controller/LoginController.java
@@ -1,24 +1,15 @@
 package com.dbs.sae.training.nerddinner.controller;
 
 import com.dbs.sae.training.nerddinner.model.RegisterAccount;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Locale;
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
-
-    private final MessageSource messageSource;
-
-    @Autowired
-    public LoginController(MessageSource ms) {
-        this.messageSource = ms;
-    }
-
 
     @GetMapping(Paths.Login.forgotPasswordPath)
     public String forgotPassword() {
@@ -31,15 +22,16 @@ public class LoginController {
     }
 
     @GetMapping(Paths.Login.registerAccountPath)
-    public String registerAccount(final RegisterAccount registerAccount, Locale locale) {
+    public String registerAccount(final RegisterAccount registerAccount) {
 
         return "login/registerAccount";
     }
 
     @PostMapping(Paths.Login.registerAccountPath)
-    public String postRegisterAccount(RegisterAccount registerAccount) {
-
-
+    public String postRegisterAccount(@Valid RegisterAccount registerAccount, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "login/registerAccount";
+        }
         return "redirect:/";
     }
 
--- src/main/java/com/dbs/sae/training/nerddinner/model/RegisterAccount.java
+++ src/main/java/com/dbs/sae/training/nerddinner/model/RegisterAccount.java
@@ -1,13 +1,27 @@
 package com.dbs.sae.training.nerddinner.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
+@Matches(field = "password", verifyField = "confirmPassword")
 public class RegisterAccount {
+
+    @NotEmpty()
     private String firstName;
+
+    @NotEmpty()
     private String lastName;
+
+    @NotEmpty()
     private String email;
+
+    @NotEmpty()
     private String userName;
+
+    @NotEmpty()
     private String password;
+
+    @NotEmpty()
     private String confirmPassword;
 }
